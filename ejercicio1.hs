bd21d59351dbfc2bbc1ff5221e78670db830833f
SevroAuBarca <daniel_contreras001@hotmail.com>
2022-09-30 12:07:04 -0500

    ejercicios haskell

---
+++ ejercicio1.hs
@@ -0,0 +1,7 @@
+
+vacaciones nLlavero nPlayera nImagen nLibro = (nLlavero * 12) + (nPlayera * 45) + (nImagen * 125) + (nLibro * 120) 
+
+hombres_en_batalla muertos heridos utiles x = ((x/muertos) + (x/heridos) + (x/utiles) )* ((x/muertos) + ((x/heridos) + 60) + ((x/utiles) + 820))
+
+total_gastado = vacaciones 10 5 1 2
+total_hombres = hombres_en_batalla 20 12 2 1
